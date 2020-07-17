ad9a9a858ba46ddfa48224887001d99b76f2480c
SatishC16 <satishchandan786@gmail.com>
2020-07-17 13:47:09 +0530

    Updates

---
+++ precache-manifest.5866c1a199e586d600025e6d895384ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fde74321ae66e79ee74627947aec89d2",
+    "url": "/pomodoro-timer/index.html"
+  },
+  {
+    "revision": "86ce00fb09588f5341d0",
+    "url": "/pomodoro-timer/static/css/main.540a1c28.chunk.css"
+  },
+  {
+    "revision": "33ec6890519b71d05645",
+    "url": "/pomodoro-timer/static/js/2.b6dfed0f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pomodoro-timer/static/js/2.b6dfed0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86ce00fb09588f5341d0",
+    "url": "/pomodoro-timer/static/js/main.42e50487.chunk.js"
+  },
+  {
+    "revision": "e9d05b375a36adfea9f6",
+    "url": "/pomodoro-timer/static/js/runtime-main.1a3fb540.js"
+  }
+]);
